c737c15c270b593d05509881a7ecddbd3aeae96e
Kelsey Krehbiel <kelsey.krehbiel@verramobility.com>
2021-12-17 16:39:44 -0700

    add giantsquid

---
+++ GiantSquid.ps1
@@ -0,0 +1,7 @@
+<#
+Bingo
+Bingo is played on a set of boards each consisting of a 5x5 grid of numbers. 
+Numbers are chosen at random, and the chosen number is marked on all boards on which it appears. 
+(Numbers may not appear on all boards.) 
+If all numbers in any row or any column of a board are marked, that board wins. (Diagonals don't count.)
+#>
