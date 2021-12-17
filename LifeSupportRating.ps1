68376ae1673cff212f9e838c60da541f880d3842
Kelsey Krehbiel <kelsey.krehbiel@verramobility.com>
2021-12-17 15:20:44 -0700

    add data filter functions

--- LifeSupportRating.ps1
+++ LifeSupportRating.ps1
@@ -16,3 +16,83 @@ The bit criteria depends on which type of rating value you want to find:
     If 0 and 1 are equally common, keep values with a 0 in the position being considered.
 
 #>
+
+$ReportData = Get-Content .\ReportData.txt
+
+function Get-OxygenGeneratorRating() {
+    [CmdletBinding()]
+    param (
+        $OGRData
+    )
+    #To find oxygen generator rating, determine the most common value (0 or 1) in the current bit position, 
+    #If 0 and 1 are equally common, keep values with a 1 in the position being considered.
+    #keep only numbers with that bit in that position.
+
+    Write-Verbose "Finding OGR"
+    foreach($position in (0..($OGRData[0].Length-1))){
+        Write-Verbose "Checking position $position"
+        $1count = 0
+        foreach($item in $OGRData){
+            if($item[$position] -eq '1'){
+                $1count += 1
+            }
+        }
+        if($1count -ge $OGRData.count/2){
+            #remove numbers with 0 in current position
+            Write-Verbose "1 is most common in position $position"
+            $OGRData = $OGRData | Where-Object{$_[$position] -eq '1'}
+        }
+        else{
+            #remove numbers with 1 in current position
+            Write-Verbose "0 is most common in position $position"
+            $OGRData = $OGRData | Where-Object{$_[$position] -eq '0'}
+        }
+        Write-Verbose "$($OGRData.count) items remain"
+        if($OGRData.count -eq 1){
+            $OGRData
+        
+            break
+        }
+    }
+
+}
+
+function Get-ScrubberRating() {
+    #To find CO2 scrubber rating, determine the least common value (0 or 1) in the current bit position.
+    #If 0 and 1 are equally common, keep values with a 0 in the position being considered.
+    #keep only numbers with that bit in that position. 
+    [CmdletBinding()]
+    param(
+        $ScrubData
+    )
+    Write-Verbose "Finding Scrub"
+    foreach($position in (0..($ScrubData[0].Length-1))){
+        Write-Verbose "Checking position $position"
+        $1count = 0
+        foreach($item in $ScrubData){
+            if($item[$position] -eq '1'){
+                $1count += 1
+            }
+        }
+        if($1count -lt $ScrubData.count/2){
+            #remove numbers with 0 in current position
+            Write-Verbose "1 is least common in position $position"
+            $ScrubData = $ScrubData | Where-Object{$_[$position] -eq '1'}
+        }
+        else{
+            #remove numbers with 1 in current position
+            Write-Verbose "0 is least common in position $position"
+            $ScrubData = $ScrubData | Where-Object{$_[$position] -eq '0'}
+        }
+        Write-Verbose "$($ScrubData.count) items remain"
+        if($ScrubData.count -eq 1){
+            $ScrubData
+        
+            break
+        }
+    }
+}
+$OGbstring = Get-OxygenGeneratorRating -OGRData $ReportData
+$CObstring = Get-ScrubberRating -ScrubData $ReportData
+Write-Host "Oxygen Generation Rating:$OGbstring"
+Write-Host "CO2 Scrub Rating:$CObstring"
