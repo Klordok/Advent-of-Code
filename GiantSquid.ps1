8768249d5f33d7f8b80ed9883f35610e0e5011d4
Kelsey Krehbiel <kelsey.krehbiel@verramobility.com>
2021-12-30 16:34:00 -0700

    index matching

--- GiantSquid.ps1
+++ GiantSquid.ps1
@@ -13,15 +13,32 @@ To guarantee victory against the giant squid, figure out which board will win fi
 What will your final score be if you choose that board?
 #>
 
-$bingoInput = (Get-Content .\bingoInput.txt -Raw)
+$bingoInput = Get-Content .\bingoInput.txt -Raw
 
 $splitInput = $bingoInput -split "\r?\n\r?\n"
 
-$numberList = $splitInput[0]
+$numberList = $splitInput[0].Split(',')
 
-$boardList = $splitInput[1..($splitInput.count-1)]
+$allBoardStrings = $splitInput[1..($splitInput.count-1)]
 
-function Check-Board {
+#convert boards to multidimentional arrays
+$allBoards = New-Object -TypeName "System.Collections.ArrayList"
+
+foreach($boardString in $allBoardStrings){
+    $board = New-Object -TypeName "System.Collections.ArrayList"
+    $rowStrings = $boardString.Split("`n")
+    foreach ($row in $rowStrings) {
+        #convert row to an int array
+        $rowList = @()
+        [int[]]$rowValues = $row.trim().split()
+        $rowList += $rowValues
+        $board.Add($rowList)
+    }
+    #creates a 3 dimensional array. board,row,column
+    $allBoards.Add($board)
+}
+
+function Search-Board {
     #Check board for number
     #Mark number
     #Check for bingo
@@ -29,14 +46,23 @@ function Check-Board {
         $board,
         $number
     )
-    
 }
 
 foreach($number in $numberList){
     #check each board for number
     #mark number. (change symbol?)
     #Check if bingo
-    foreach($board in $boardList){
-        Check-Board
+    foreach($boardIndex in 0..99){
+        #search each board for matching number
+        if($allBoards[$boardIndex] -match $number){
+            #search each row in board
+            foreach ($rowIndex in 0..4) {
+                $column = $allBoards[$boardIndex][$rowIndex].IndexOf($number)
+                #if number exists get the index and mark the board
+                if($column -ne -1){
+                    $allBoards[$boardIndex][$rowIndex][$column] = 'x'
+                }
+            }
+        }    
     }
 }
