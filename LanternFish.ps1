4270d1b149e841d732bb211982c8b0e84107c04a
Klordok <kelsey@kelseykrehbiel.com>
2022-01-09 16:42:45 -0700

    lanternfish correct

--- LanternFish.ps1
+++ LanternFish.ps1
@@ -14,16 +14,48 @@ In this example, after 18 days, there are a total of 26 fish. After 80 days, the
 
 Find a way to simulate lanternfish. How many lanternfish would there be after 80 days?
 #>
+
 $InitialState = Get-Content -Path .\LanternFishData.txt
 
 #For each day:
 #0 becomes 6
 #add new 8 to list
 #decrease all other numbers by 1
-
-function Age-Fish {
-  param (
-    $FishAges
-  )
-  
+$FishState = New-Object -TypeName "System.Collections.ArrayList"
+$FishList = $InitialState.Split(",")
+foreach ($fish in $FishList) {
+  #make has of fish and spawn state
+  $FishState.Add([PSCustomObject]@{
+    Age = [int]$fish
+  }) | Out-Null
 }
+
+function Age-Fish() {
+  #Age fish 1 cycle
+  $NewFish = 0
+  foreach ($fish in $FishState) {
+    #fish that are born get to rest this cycle
+    if($fish.age -eq 0) {
+      $fish.age = 6
+      $NewFish += 1
+    }
+    else {
+      $fish.Age = $fish.Age-1
+    }
+  }
+  if($NewFish -ge 1){
+    foreach($baby in 1..$NewFish){
+      $FishState.Add([PSCustomObject]@{
+        Age = 8
+      }) | Out-Null
+    }
+  }
+}
+
+Write-Host "Starting with $($FishState.Count) fish"
+foreach ($day in 1..80) {
+  Age-Fish
+  Write-Host "Day $day : $($FishState.Count)"
+}
+Write-Host "Answer: $($FishState.Count) fish"
+#Answer: 355386
