5a03ce0e261faf8a90c9022bd579637b54969f5d
Kelsey Krehbiel <kelsey.krehbiel@verramobility.com>
2022-01-14 16:36:31 -0700

    start recursive function

--- LanternFish2.ps1
+++ LanternFish2.ps1
@@ -21,71 +21,17 @@ $FishList = $TestState.Split(",")
 $FishState = [List[int]]@($FishList)
 $days = 1
 
-function Count-Children {
-    param(
-        $daysToSpawn,
-        $duration
-    )
-    #how many children after x days?
-    $spawnDays = $duration - $daysToSpawn
-    $children = [System.Math]::ceiling($spawnDays/7)
-    $children
+function Count-Descendants(){
+  param(
+    $daysToSpawn,
+    $RemainingDays
+  )
+  # (Children of Gen1)*(Children of Gen 2)...until no more days
+  $children = [Math]::Ceiling(($RemainingDays - $daysToSpawn)/7)
+  if($RemainingDays - $daysToSpawn -ge 1){
     
+  }  
 }
-
-function Count-Descendants() {
-  $Descendants = 0
-  foreach ($fish in $FishState){
-    $Descendants += (Count-Children -daysToSpawn $fish -duration $days)
-  }
-  Write-Host "$Descendants"
-}
-
-function Explode-Fish {
-  param (
-    [int]$daysToSpawn,
-    [int]$duration
-  )
-  $spawnDays = $duration - $daysToSpawn
-  $cycles = [Math]::Ceiling($spawnDays/7)
-  $population = $daysToSpawn*([Math]::Pow(2,$cycles))
-  Write-Host "Explode-Fish counted $population fish after $days days" -ForegroundColor Green
-}
-
-function Age-Fish() {
-  #Age fish 1 cycle
-  $NewFish = 0
-  foreach ($fish in 0..($FishState.Count - 1)) {
-    #fish that are born get to rest this cycle
-    if($FishState[$fish] -eq 0) {
-      $FishState[$fish] = 6
-      $NewFish += 1
-    }
-    else {
-      $FishState[$fish] = $FishState[$fish]-1
-    }
-  }
-  if($NewFish -ge 1){
-    foreach($baby in 1..$NewFish){
-      $FishState.Add(8)
-    }
-  }
-}
-
-function Grow-Population {
-  #calculate population after x days
-  param (
-    $days
-  )
-  Write-Host "Starting with $($FishState.Count) fish"
-  foreach ($day in 1..$days) {
-    Age-Fish
-    #Write-Host "Day $day : $($FishState.Count)"
-  }
-  Write-Host "Grow-Pop counted $($FishState.Count) fish after $days days" -ForegroundColor Green
-}
-Explode-Fish -fish $FishState.Count -days $days
-Grow-Population -days $days
 
 #Answer:
 
